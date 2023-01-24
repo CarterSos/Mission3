985ed6ea191d432b3c2bd946f6181fdc48a76995
CarterSos <carterjwalch@gmail.com>
2023-01-24 09:28:46 -0700

    Driver program final changes

--- Mission3/Program.cs
+++ Mission3/Program.cs
@@ -7,19 +7,61 @@ namespace Mission3
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
-            string[] gameBoard = { " ", " ", " ", " ", " ", " ", " ", " ", " "};
+            string[] gameBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" }; // array contains 0-9 and makes no use of index 0
+            int playerCount = 1;
+            bool keepPlaying = true;
 
-            Console.WriteLine("Player 1: ");
-            int choice = Console.ReadLine();
-            gameBoard[choice] = 'x'
-            
-            printBoard(gameBoard);
+            while (keepPlaying)
+            {
 
-            Console.WriteLine("Player 2: ");
-            int choice = Console.ReadLine();
-            gameBoard[choice] = 'o'
+                if (playerCount % 1 == 1) // alternates between player 1 and player 2 based on odd or even playerCount
+                {
+                    Console.WriteLine("Player 1's Turn: ");
+                }
 
-            printBoard(gameBoard);
+                else
+                {
+                    Console.WriteLine("Player 2's Turn: ");
+                }
+
+                // now get choice from user (number 1 through 9)
+                int choice = Console.ReadLine();
+
+                if (gameBoard[choice] == "X" || gameBoard[choice] == "O") // checks if spot is already taken
+                {
+                    Console.WriteLine("Sorry, this spot is already taken!");
+                }
+
+                else // else replace that spot with X or O
+                {
+                    if (playerCount % 1 == 1)
+                    {
+                        gameBoard[choice] = 'X';
+                        printBoard(gameBoard);
+                        playerCount++; // increments playerCount to go to the next player's turn
+                    }
+
+                    else
+                    {
+                        gameBoard[choice] = 'O';
+                        printBoard(gameBoard);
+                        playerCount++; // increments playerCount to go to the next player's turn
+                    }
+                }
+
+                chooseWinner(gameBoard);
+
+                if (chooseWinner(gameBoard) == "(Player 1 wins!)" || chooseWinner(gameBoard) == "Player 2 wins!")
+                {
+                    keepPlaying = false;
+                }
+
+                else
+                {
+                    keepPlaying = true;
+                }
+
+            } // end of while loop
         }
     }
 }
